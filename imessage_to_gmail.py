5f3d8d0b1fa851d002c5f42562155378575a1a11
Stephen Fegan <sfegan@gmail.com>
2017-01-06 14:22:04 +0100

    Start python transfer program

---
+++ imessage_to_gmail.py
@@ -0,0 +1,21 @@
+# imessage_to_gmail.py - Backup messages from iMessage under Mac OS to GMail
+#
+# Stephen Fegan - sfegan@gmail.com - 2017-01-06
+#
+# This program is motivated by the author's experience of SMSBackup+ under
+# Android, an excellent application to backup SMS/MMS messages to GMail where
+# they can be searched etc. This little program tries to do the same thing for
+# messages / conversations stored in the iMessage database.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
