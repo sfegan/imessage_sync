60a1f2608c8b9a78c25f37aa0f39244b6f39e380
Stephen Fegan <sfegan@gmail.com>
2017-03-01 15:47:12 +0100

    Start adding code to read Mac OS Address Book

--- addressbook.py
+++ addressbook.py
@@ -20,12 +20,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sqlite3
+import phonenumbers
+import os
+
+ab_base_dir = '~/Library/Application Support/AddressBook'
+ab_source_dir = 'Sources'
+ab_db_file = 'AddressBook-v22.abcddb'
+
 class AddressBook:
-    def __init__(self, me):
-        self._me = me
+    def __init__(self, my_identity, default_country_code=None):
+        self._me = my_identity
+        self._cc = default_country_code or 'FR'
 
     def me(self):
         return self._me
+
+    def read_address_db(self, filename = ab_base_dir + '/' + ab_db_file):
+        ab = dict()
+        db = sqlite3.connect('file:'
+            + os.path.expanduser(filename)
+            + '?mode=ro', uri=True)
+        query = db.cursor()
+
+        for entry in query.execute('SELECT Z_PK, ZFIRSTNAME, ZMIDDLENAME, '
+                'ZLASTNAME, ZNICKNAME, ZORGANIZATION FROM ZABCDRECORD'):
+            ab[entry[0]] = dict(
+                owner_id                = entry[0],
+                first_name              = entry[1],
+                middle_name             = entry[2],
+                last_name               = entry[3],
+                nick_name               = entry[4],
+                organization            = entry[5],
+                phone_numbers           = dict(),
+                email_addresses         = dict()
+                )
+
+        for entry in query.execute('SELECT ZOWNER, ZORDERINGINDEX, '
+                'ZFULLNUMBER FROM ZABCDPHONENUMBER'):
+            ph = phonenumbers.parse(entry[2], self._cc)
+            ph = '+' + str(ph.country_code) + str(ph.national_number)
+            ab[entry[0]]['phone_numbers'][entry[1]] = ph
+
+        for entry in query.execute('SELECT ZOWNER, ZORDERINGINDEX, '
+                'ZADDRESS FROM ZABCDEMAILADDRESS'):
+            ab[entry[0]]['email_addresses'][entry[1]] = entry[2]
+        return ab
 
     def lookup_email(self, handle):
         return handle['contact'] + ' <' + handle['contact'] + '@unknown.email.local>'
